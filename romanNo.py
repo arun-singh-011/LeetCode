c6b54854a3a4c5658c8ef89301f84a90b01f8353
ARUN SINGH <arunsinghh0011@gmail.com>
2025-02-15 23:03:32 -0800

    Problem #12

--- romanNo.py
+++ romanNo.py
@@ -2,19 +2,19 @@ class Solution:
     def romanToInt(self, s: str) -> int:
       #    largest to smallest: add them up
       #    smaller before larger: subtract smaller
-     
-     
-      roman={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+      roman={"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D" : 500, "M" : 1000}
 
       res = 0
- 
+
       for i in range(len(s)):
-          if i+1<len(s) and roman[s[i]]<roman[s[i+1]]:
-             res-=roman[s[i]]
+          if i + 1 < len(s) and roman[s[i]] < roman[s[i+1]]:
+             res -= roman[s[i]]
+             print(roman[s[i]])
           else:
-             res+=roman[s[i]]
-            
-            
+             res += roman[s[i]] 
+    
+
       print(res)
-      return res
+     
+    
     romanToInt(self=0,s="III")
