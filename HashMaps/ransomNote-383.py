ef6b023b170d3d64aa10c283339bfae19f109641
ARUN SINGH <arunsinghh0011@gmail.com>
2025-02-27 00:24:39 -0800

    solution of ransom note

---
+++ HashMaps/ransomNote-383.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        
+        
+        counter ={} # VALUE: INDEX
+
+        for c in magazine:
+            if c in counter:
+                counter[c] += 1
+            else:
+                counter[c] = 1
+        
+        for c in ransomNote:
+            if c not in counter:
+                return False
+            elif counter[c] == 1:
+                del counter[c]
+            else:
+                counter[c] -= 1
+        return True
+
+
+    canConstruct(self=0, ransomNote="aab", magazine="baa")
