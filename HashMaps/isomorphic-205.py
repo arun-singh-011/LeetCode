dd812707ce0e185b7529a6ecef23b0f28e3e236c
ARUN SINGH <arunsinghh0011@gmail.com>
2025-02-28 02:56:39 -0800

    s

---
+++ HashMaps/isomorphic-205.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        
+       mapST, mapTS = {}, {}
+
+       for i in range(len(s)):
+        c1, c2 = s[i], t[i]
+        if ((c1 in mapST and mapST[c1] != c2)) or ((c2 in mapTS and mapTS[c2] != c1)):
+           return False
+
+        mapST[c1] = c2
+        mapTS[c2] = c1
+        return True
+
+        
+        
+        print(s)
+        print(t)
+        
+    isIsomorphic(self=0, s="egg", t ="add")
