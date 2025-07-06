6ce6a4aa2986dca80ae2d9e1cf49d538bdc5072c
ARUN SINGH <arunsinghh0011@gmail.com>
2025-07-05 23:22:08 -0700

    day 1 of leetcode

---
+++ Array/mergeSortedArrray-88.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        l = nums1 - 1
+        r = nums2 - 1
+
+        if nums1[l] > nums2[r]:
+            nums1[l] = l
+
