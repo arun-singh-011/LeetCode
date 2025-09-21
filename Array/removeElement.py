8b5dfc9c46f8f12bd8cc71c762041aa5dde5a910
ARUN SINGH <arunsinghh0011@gmail.com>
2025-09-20 21:35:00 -0700

    solution to problem number 27 remove element

--- Array/removeElement.py
+++ Array/removeElement.py
@@ -2,18 +2,15 @@ from typing import List
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
          
-        l = 0
-       
-        while l < len(nums):
-            if nums[l] == val:
-                nums.remove(nums[l])
-                l += 1
-                print("length of array", len(nums))
-                 
-                print(nums)
-            elif nums[l] != val:
-                l += 1
-              
-        return print(len(nums))
+     l = 0
+     i = 0
+
+     for l in range(0,(len(nums))):
+        if nums[l] != val:
+           nums[i] = nums[l]
+           i += 1
+     print(i)
+     
+ 
 
     removeElement(nums=[0,1,2,2,3,0,4,2],val=2,self=0)
