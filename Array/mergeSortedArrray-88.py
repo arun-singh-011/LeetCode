b523fbe854d764fcac7f00582125a115065059d7
ARUN SINGH <arunsinghh0011@gmail.com>
2025-09-20 20:42:33 -0700

    Answer to merge sorted array problem number 88

--- Array/mergeSortedArrray-88.py
+++ Array/mergeSortedArrray-88.py
@@ -6,9 +6,28 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        # l = nums1 - 1
-        # r = nums2 - 1
+        print(nums1)
+        # last index nums1
+        last = m + n - 1
+        print(nums1[m-1])
+        print(nums2[n-1])
 
-        # if nums1[l] > nums2[r]:
-        #     nums1[l] = l
+        # merge in reverse order
+        while m > 0 and n > 0:
+          if nums1[m - 1] > nums2[n - 1]:
+              nums1[last] = nums1[m - 1]
+              m -= 1
+          else:
+              nums1[last] = nums2[n - 1]
+              n -= 1
+          last -= 1
+
+        #  fill nums1 with leftover nums2 elements
+        while n > 0:
+           nums1[last] = nums2[n - 1]
+           n = n - 1
+           last = last - 1
+         
+ 
+    merge(self = 0, nums1 = [1,2,3,0,0,0], nums2 = [2,5,6], n = 3 , m = 3)
 
