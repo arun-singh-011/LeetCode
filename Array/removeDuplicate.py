a262dfc532dad4e1107404606ae7ceb6e03d1ae0
ARUN SINGH <arunsinghh0011@gmail.com>
2025-09-20 22:25:02 -0700

    solution to the problem number 26 remove duplicate value from sorted array

--- Array/removeDuplicate.py
+++ Array/removeDuplicate.py
@@ -2,19 +2,18 @@ from typing import List
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         l = 1
-        r = 1
+   
 
         for r in range(1,len(nums)):
             if nums[r] != nums[r-1]:
                 nums[l] = nums[r]
-                r += 1
+            
                 l += 1
                 
-            else:
-                r += 1
+            
            
        
-            print(l)
+        print(l)
       
 
     removeDuplicates(self=0,nums=[1,1,1,2,2,2,3,3,4,4,4,4])
